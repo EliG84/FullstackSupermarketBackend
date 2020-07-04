f56c8450cb3aa5ad7aa577dd85743cd9f5f40bc4
Eli <elionlink@gmail.com>
2020-07-04 23:48:58 +0300

    04072020

--- Models/profile.js
+++ Models/profile.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProfileSchema = new Schema({
-  FirstName: { type: String, default: '' },
-  LastName: { type: String, default: '' },
-  street: { type: String, default: '' },
-  country: { type: String, default: '' },
-  dob: { type: Date, default: '' },
+  FirstName: { type: String, default: 'Jon' },
+  LastName: { type: String, default: 'Doe' },
+  street: { type: String, default: '12 Main St.' },
+  country: { type: String, default: 'Mars Colony 001' },
+  dob: { type: Date, default: Date.now() },
   image: { type: String, default: 'https://picsum.photos/200' },
 });
 
