d6e964b2ed9d543eb68c6dd3ba48007a30255596
Eli <elionlink@gmail.com>
2020-07-06 21:27:53 +0300

    06072020

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const path = require('path');
 const fileUpload = require('express-fileupload');
 const mongoConnect = require('./DB/mongoConnect');
@@ -9,20 +9,20 @@ const port = process.env.PORT;
 
 mongoConnect();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,POST,PATCH,PUT,DELETE,OPTIONS'
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin,X-Requested-With,Content-Type,Accept'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET,POST,PATCH,PUT,DELETE,OPTIONS'
+//   );
+//   next();
+// });
 
-// app.use(cors());
+app.use(cors());
 app.use(fileUpload());
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.json());
